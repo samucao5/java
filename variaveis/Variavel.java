e8737cd7140b8975b5135f0e9e322ea078e7d931
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-07-22 07:22:09 -0300

    Add files via upload

---
+++ variaveis/Variavel.java
@@ -0,0 +1,26 @@
+//Define que a classe pertence ao pacote variaveis.
+package variaveis;
+
+//Declara uma classe pública chamada Variavel.
+public class Variavel {
+
+    //É o método principal, onde o programa começa a ser executado.
+    public static void main(String[] args)
+    {
+        /*o que sao variaveis  
+        declaracao de variaveis
+        tipo -> nome_da_variavel -> atribuir um valor;
+        */
+        //declarando o nome
+        String nome = "samuel";
+        //declarando a idade
+        int idade = 20;
+
+        System.out.println(nome);
+        
+        System.out.println(idade);
+
+    }
+
+
+}
