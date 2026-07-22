a72223ca4b4eac64892985301b16bf8217e0f00b
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-07-22 08:01:38 -0300

    Add files via upload

---
+++ variaveis/Long.java
@@ -0,0 +1,14 @@
+package variaveis;
+
+public class Long {
+    
+    public static void main(String[] args) 
+    {
+        long valor = 4000003040l;
+        System.out.println(valor);
+        
+        long valor_1 = valor + 1;
+        System.out.println(valor_1);
+    }
+}
+
