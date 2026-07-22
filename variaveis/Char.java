be1d122a787e6f53b9c5f7fe261f971d05319e3b
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-07-22 07:52:57 -0300

    Add files via upload

---
+++ variaveis/Char.java
@@ -0,0 +1,13 @@
+package variaveis;
+
+public class Char {
+    public static void main(String[] args) 
+    {
+        char letra = 's';
+        System.out.println(letra);
+        char numero = '5';
+        System.out.println(numero);
+        
+    }
+
+}
