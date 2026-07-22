60b8df1d0702dcd050e87a1218fb9f5a4649391f
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-07-22 07:53:13 -0300

    Add files via upload

---
+++ variaveis/Int.java
@@ -0,0 +1,18 @@
+package variaveis;
+
+public class Int 
+{
+    public static void main(String[] args) 
+    {
+        int numero = 5;
+
+        System.out.println(numero * 5);
+        System.out.println(numero + 3);
+        System.out.println(numero / 5);
+
+        int sub = numero - 4;
+        System.out.println(sub);
+    
+    }
+}
+    
