8339277a85b750451608e9e5ccf6fe6b1a1d108c
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-07-22 07:34:41 -0300

    Add files via upload

---
+++ variaveis/strings.java
@@ -0,0 +1,18 @@
+package variaveis;
+
+public class strings{
+
+    public static void main(String[] args) {
+        String nome = "Samuel";
+        String sobrenome = "Felipe";
+        String estado = "Rio de Janeiro";
+        String nome_completo = nome + " " + sobrenome;
+
+        System.out.println("nome: " + nome + "\nsobrenome: " + sobrenome);
+        System.out.println("estado: " + estado);
+        System.out.println("nome completo: " + nome_completo);
+        
+
+    }
+
+}
