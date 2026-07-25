a608123c578947747be32a38c4d9cb182b14489b
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-07-25 12:40:39 -0300

    Add files via upload

---
+++ variaveis/var.java
@@ -0,0 +1,19 @@
+package variaveis;
+
+public class Var{
+    public static void main(String [] args)
+    {
+        var valor = 5;
+        
+        System.out.println(valor);
+
+        var texto = "teste";
+
+        var double_1 = 2.5;
+
+        System.out.println(texto);
+
+        System.out.println(double_1);
+
+    }
+}
