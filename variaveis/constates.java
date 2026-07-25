28d4c41b22e8001631ecb7815b551b0a1c14d68a
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-07-25 12:11:09 -0300

    Add files via upload

---
+++ variaveis/constates.java
@@ -0,0 +1,11 @@
+package variaveis;
+
+public class constates{
+    public static void main(String [] args)
+    {
+        //constantes 
+        final float pi = 3.14f;
+        System.out.println("valor de pi: " + pi);
+        
+    }
+}
