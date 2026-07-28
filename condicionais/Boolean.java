205471913ecbe6e4f9a2124594533f6fa15a5f93
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-07-27 21:04:12 -0300

    Add files via upload

---
+++ condicionais/Boolean.java
@@ -0,0 +1,13 @@
+package condicionais;
+
+public class Boolean {
+    public static void main(String [] args){
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+
+
+    }
+}
