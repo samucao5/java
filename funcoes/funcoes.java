d17c7efad6c024d7a68b88b490bbe2f3269348a1
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-09-19 15:57:06 -0300

    Add files via upload

---
+++ funcoes/funcoes.java
@@ -0,0 +1,14 @@
+package funcoes;
+
+public class funcoes
+{
+    public static void main(String[] args)
+    {
+        saudacao();
+    }
+
+    public static void saudacao()
+    {
+        System.out.println("ola");
+    }
+}
